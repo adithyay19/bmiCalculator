4d35177f11f78ff141af4f757df8067f547f28f7
adithyay19 <adithya842@gmail.com>
2024-10-02 20:24:17 +0530

    Added basic functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState(0);
+  // const [weightUnit, setWeightUnit] = useState("");
+  // const [heightUnit, setHeightUnit] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(weight, height);
+    const value = Number(weight)/(Number(height) * Number(height) / 10000);
+    setBmi(value);
+    setWeight("");
+    setHeight("");
+  }
 
   return (
     <>
+      <header>
+        <h2>BMI Calculator</h2>
+      </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="weight"
+            type="number"
+            label={`Weight (in kg)`}
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+          <TextField
+            id="height"
+            type="number"
+            label={`Height (in cm)`}
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Calculate
+          </Button>
+        </form>
+        <div>
+          <h3>{bmi}kg/m2</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Data.tsx
@@ -0,0 +1,42 @@
+export const classifications = [
+    {
+        classification : "Severe Thinness",
+        upperLimit : 16,
+        lowerLimit : 0,
+    },
+    {
+        classification: "Moderate Thinness",
+        upperLimit: 16,
+        lowerLimit: 17,
+    },
+    {
+        classification: "Mild Thinness",
+        upperLimit: 17,
+        lowerLimit: 18.5,
+    },
+    {
+        classification: "Normal",
+        upperLimit: 18.5,
+        lowerLimit: 25,
+    },
+    {
+        classification: "Overweight",
+        upperLimit: 25,
+        lowerLimit: 30,
+    },
+    {
+        classification: "Obese Class I",
+        upperLimit: 30,
+        lowerLimit: 35,
+    },
+    {
+        classification: "Obese Class II",
+        upperLimit: 35,
+        lowerLimit: 40,
+    },
+    {
+        classification: "Obese Class III",
+        upperLimit: 40,
+        lowerLimit: 1000,
+    }
+]
